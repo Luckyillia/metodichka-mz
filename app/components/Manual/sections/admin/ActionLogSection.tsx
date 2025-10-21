bcd74eebc4e944fae06390dfe6cb819e05176d09
luckyillia <zajchenkoi2007@gmail.com>
2025-10-22 01:51:49 +0200

    Bug fix data on section ActionLog

--- app/components/Manual/sections/admin/ActionLogSection.tsx
+++ app/components/Manual/sections/admin/ActionLogSection.tsx
@@ -72,6 +72,7 @@ export default function ActionLogSection() {
             day: "2-digit",
             month: "2-digit",
             year: "numeric",
+            hour: "2-digit",
             minute: "2-digit",
             second: "2-digit",
         }).format(date)
